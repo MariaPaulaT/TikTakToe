5dbc7dab3feaaaa412a4cd850227f37063615440
MariaPaulaT <mp.tellez@uniandes.edu.co>
2023-02-09 20:30:22 -0500

    Adding sounds to the game. Tik tak toe completed

--- game.js
+++ game.js
@@ -8,6 +8,7 @@ var actualPlayer = 0;
 $(document).keypress(function(){
     if(started === false){
         $("h2").text("It´s player 1 turn!");
+        $("h3").text("");
         actualPlayer = 1;
         started = true;
     }   
@@ -28,19 +29,30 @@ function playersTurn (buttonChosen){
         if (!player1Cards.includes(buttonChosen) && !player2Cards.includes(buttonChosen)){
             $("#"+buttonChosen).addClass("circle");
             player1Cards.push(buttonChosen);
-            
+            var audio = new Audio("sounds/" + "sounds_red" + ".mp3");
+            audio.play();
+
             checkAnswer (player1Cards,actualPlayer);
             actualPlayer=2;
             
+        }else{
+            var audio = new Audio("sounds/" + "sounds_wrong" + ".mp3");
+            audio.play();
         }
     } else{
         if (!player1Cards.includes(buttonChosen) && !player2Cards.includes(buttonChosen)){
         
         $("#"+buttonChosen).addClass("cerca");
         player2Cards.push(buttonChosen);
+        var audio = new Audio("sounds/" + "sounds_yellow" + ".mp3");
+            audio.play();
+
         checkAnswer (player2Cards,actualPlayer);
         actualPlayer=1;
         
+        }else{
+            var audio = new Audio("sounds/" + "sounds_wrong" + ".mp3");
+            audio.play();
         }
     }
 }
@@ -48,21 +60,30 @@ function playersTurn (buttonChosen){
 function checkAnswer (playerCards, actualPlayer){
     if(playerCards.includes("one")&& playerCards.includes("two")&& playerCards.includes("three")|| playerCards.includes("four")&& playerCards.includes("five")&& playerCards.includes("six")
     || playerCards.includes("seven")&& playerCards.includes("eight")&& playerCards.includes("nine")|| playerCards.includes("one")&& playerCards.includes("four")&& playerCards.includes("seven")|| playerCards.includes("two")&& playerCards.includes("five")&& playerCards.includes("eight")
-    || playerCards.includes("three")&& playerCards.includes("six")&& playerCards.includes("nine")){
+    || playerCards.includes("three")&& playerCards.includes("six")&& playerCards.includes("nine")|| playerCards.includes("one")&& playerCards.includes("five")&& playerCards.includes("nine") || playerCards.includes("three")&& playerCards.includes("five")&& playerCards.includes("seven")){
         $("h2").text("⭐ Player "+ actualPlayer + " Wins! ⭐");
 
         setTimeout(function(){
             $(".btn").removeClass("circle");
             $(".btn").removeClass("cerca");
         },500)
-
-    }else if(player1Cards.length + player2Cards.length === 9){
+        started = false;
+        $("h2").after("<h3>Press A key to start again!</h3>");
+        player1Cards=[];
+        player2Cards=[];
+   }else if(player1Cards.length + player2Cards.length === 9){
         $("h2").text("It's a tie! 😕");
+        var audio = new Audio("sounds/" + "sounds_wrong" + ".mp3");
+            audio.play();
         setTimeout(function(){
             $(".btn").removeClass("circle");
             $(".btn").removeClass("cerca");
         },500)
 
+        started = false;
+        $("h2").after("<h3>Press A key to start again!</h3>");
+        player1Cards=[];
+        player2Cards=[];
     }
     else if(actualPlayer===1){
         actualPlayer=2;
@@ -71,4 +92,9 @@ function checkAnswer (playerCards, actualPlayer){
         actualPlayer=1;
         $("h2").text("It´s player 1 turn!");
     }
+}
+
+function playSound(name){
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
 }
